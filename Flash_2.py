1511df701ed71e4902ce8414edb7e991ae647d70
Nicolás Berríos Verboven <nbverboven@gmail.com>
2016-04-08 17:13:07 +0000

    Agregados comentarios

--- Flash_2.py
+++ Flash_2.py
@@ -1,3 +1,4 @@
+#Verifica que un elento se encuentre en una lista.
 def estaEn(elemento, lista):
 	flag = False
 	for i in lista:
@@ -5,6 +6,7 @@ def estaEn(elemento, lista):
 			flag = True
 	return flag
 
+#Cuenta la cantida de elementos de la lista1 presentes en la lista2.
 def cantAparicionesSub(lista1, lista2):
 	contador = 0
 	for i in lista1:
@@ -14,12 +16,17 @@ def cantAparicionesSub(lista1, lista2):
 
 #####################################################
 
+#Verifica si los elementos de una lista, de longitud 2 o mayor, se
+#encuentran ordenados de forma estrictamente decreciente.
 def decreceHastaElFinal(lista):
 	a = 0
 	while a < len(lista)-1 and lista[a] > lista[a+1]:
 		a += 1
 	return len(lista) >= 2 and a == len(lista)-1
 
+#Verifica si la lista pasada como parametro es triangular.
+#Listas del estilo [1, 2, 3, 2, 1] , [1, 2, 3, 2], [1, 3, 2, 1] son triangulares.
+#La lista vacía, las de longitud menor a 3 o aquellas de la forma [1, 2, 2, 1] no se consideran triangulares.
 def listaTriangular(lista):
 	flag = False
 	indice_actual = 0
