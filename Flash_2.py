1bf743cfd404929d2c7ee656d3e60cdf27d76cf7
berrios nicolas <nberrios@ws8.labo4>
2016-04-08 13:31:05 -0300

    Agregada la función listaTriangular

--- Flash_2.py
+++ Flash_2.py
@@ -11,3 +11,20 @@ def cantAparicionesSub(lista1, lista2):
 		if estaEn(i, lista2):
 			contador += 1
 	return contador
+
+#####################################################
+
+def decreceHastaElFinal(lista):
+	a = 0
+	while a < len(lista)-1 and lista[a] > lista[a+1]:
+		a += 1
+	return len(lista) >= 2 and a == len(lista)-1
+
+def listaTriangular(lista):
+	flag = False
+	indice_actual = 0
+	while  indice_actual < len(lista)-1 and lista[indice_actual] < lista[indice_actual+1]:
+		if decreceHastaElFinal(lista[indice_actual+1:]):
+			flag = True
+		indice_actual += 1
+	return flag
