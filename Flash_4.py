fb9b1edcf30c302e0270cd3203f7ba90536d68f2
berrios nicolas <nberrios@ws4.labo5>
2016-05-18 20:23:26 -0300

    Agregada funcion resolver

--- Flash_4.py
+++ Flash_4.py
@@ -21,6 +21,19 @@ def diagonalesOk(tablero, pos_fila, pos_columna):
 def noAmenazada(tablero, pos_fila, pos_columna):
 	return tablero[pos_fila, :].sum() == 1 and tablero[:, pos_columna].sum() == 1 and diagonalesOk(tablero, pos_fila, pos_columna)
 
+def resolver(tablero, numero_reinas, fila):
+	if numero_reinas == 0:
+		return True
+	else:
+		for columna in range(tablero.shape[1]):
+			print(tablero)
+			tablero[fila, columna] = True
+			if not (sinAmenazas(tablero, fila, columna) and resolver(tablero, numero_reinas-1, fila+1)):
+				tablero[fila][columna] = False
+			else:
+				return True
+		return False
+
 def NReinas(n):
 	tablero = np.array([[False for i in range(n)] for j in range(n)])
 	resolver(tablero, n, 0)
