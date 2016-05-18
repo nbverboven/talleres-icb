3516cfd2ae135b3f1003b863818de62fb5f5d762
berrios nicolas <nberrios@ws4.labo5>
2016-05-18 20:04:22 -0300

    Agregada funcion noAmenazada

--- Flash_4.py
+++ Flash_4.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+def noAmenazada(tablero, pos_fila, pos_columna):
+	return tablero[pos_fila, :].sum() == 1 and tablero[:, pos_columna].sum() == 1 and diagonalesOk(tablero, pos_fila, pos_columna)
+
 def NReinas(n):
 	tablero = np.array([[False for i in range(n)] for j in range(n)])
 	resolver(tablero, n, 0)
